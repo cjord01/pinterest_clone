d2c572cc3d0f1c28c782b58775b8d0e11381e29f
Clayton Jordan <clayjchicago@gmail.com>
2014-11-10 21:31:51 -0600

    Added  user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ PinterestClone::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku
+  # Note to set this to your actual host
+  config.action_mailer.default_url_options = { :host => 'http://clays-pinterest-clone.herokuapp.com/' }
 end
